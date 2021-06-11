bceedf8db2030f0c7a25283f56e93d7f533b4024
Thiago Santos <programadorthi@gmail.com>
2021-06-11 11:45:46 -0300

    touch painting

---
+++ src/main/kotlin/02_lines_and_paths/2.6_finger_painting.kt
@@ -0,0 +1,70 @@
+package `02_lines_and_paths`
+
+import androidx.compose.desktop.Window
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
+
+fun main() = Window {
+    // Has compose desktop support to multi-touch?
+    // val inProgressPaths = remember { mutableMapOf<PointerId, Path>() }
+    val recompose = currentRecomposeScope
+    val completedPaths = remember { mutableListOf<Path>() }
+    var inProgressPath by remember { mutableStateOf(Path()) }
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDragCancel = {
+                        inProgressPath = Path()
+                    },
+                    onDragEnd = {
+                        completedPaths += inProgressPath
+                        inProgressPath = Path()
+                    },
+                    onDragStart = {
+                        inProgressPath = Path().apply {
+                            moveTo(it.x, it.y)
+                        }
+                    },
+                    onDrag = { change, _ ->
+                        inProgressPath.lineTo(
+                            x = change.position.x,
+                            y = change.position.y
+                        )
+                        recompose.invalidate() // Is it fine call invalidate manually?
+                    }
+                )
+            }
+    ) {
+        for (path in completedPaths) {
+            drawPath(
+                path = path,
+                color = Color.Blue,
+                style = Stroke(
+                    width = 10f,
+                    cap = StrokeCap.Round,
+                    join = StrokeJoin.Round
+                )
+            )
+        }
+        drawPath(
+            path = inProgressPath,
+            color = Color.Blue,
+            style = Stroke(
+                width = 10f,
+                cap = StrokeCap.Round,
+                join = StrokeJoin.Round
+            )
+        )
+    }
+}
